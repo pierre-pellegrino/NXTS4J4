f111b17e6f13a6cbe2551a9d03b9eab174750c32
Pierre <pierre.pellegrino@laposte.net>
2022-01-27 07:46:19 +0100

    added readme

--- README.md
+++ README.md
@@ -1,37 +1,32 @@
-# progame
 
-Empty project.
+# The Hyper Progame
 
-## Building and running on localhost
+Site de référencement de jeux vidéo dont je n'ai pas choisi le nom.
 
-First install dependencies:
-
-```sh
-npm install
-```
-
-To create a production build:
-
-```sh
-npm run build-prod
-```
-
-To create a development build:
-
-```sh
-npm run build-dev
-```
-
-## Running
-
-```sh
-node dist/bundle.js
-```
-
-## Credits
-
-Made with [createapp.dev](https://createapp.dev/)
+Projet découverte de npm et Webpack.
 
 
-#### A changer si motivé
-- Le filtre par console ne se fait pas sur la recherche en cours mais sur le contenu de la searchbar
+## Ressources
+
+ - [API RAWG](https://rawg.io/apidocs)
+ - [Create App](https://createapp.dev/webpack)
+
+
+## Features
+
+- Affichage des jeux récents les plus populaires
+- Bouton "Show More" pour afficher plus de jeux
+- Recherche d'un jeu par nom
+- Tri des jeux par :
+    - Console
+    - Développeur
+    - Genre (Aventure, Action...)
+- Affichage d'informations détaillées sur un jeu en cliquant sur son titre
+- Affichage responsive mobile / tablette / grand écran
+
+
+## Captures d'écran
+
+![Liste des jeux](cap1.png)
+![Détail du jeu](cap2.png)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   watch: true,
@@ -15,27 +18,56 @@ const config = {
         use: 'babel-loader',
         exclude: /node_modules/
       },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'sass-loader'
+      //   ]
+      // },
       {
-        test: /\.css$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
       },
       {
         test: /\.svg$/,
         use: 'file-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html'
+    })
+  ],
 };
 
 module.exports = config;
